169e6e3ece238fce3080d1fd8208c2b5f963dadb
Arpit <arpitpandeyofc@gmail.com>
2021-09-22 23:04:28 +0530

    After Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Header from "./Header";
+import TinderCards from "./TinderCards";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Header />
+      <Router>  
+        <Switch>
+          <Route path="/chat">
+            <h1>Chat Page</h1>
+          </Route>
+          <Route path="/">
+              <TinderCards />
+          </Route>        
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDh4_D-KjP504KAs3ezhZnLXr40bjApVE0",
+    authDomain: "tinder-clone-53e3b.firebaseapp.com",
+    projectId: "tinder-clone-53e3b",
+    storageBucket: "tinder-clone-53e3b.appspot.com",
+    messagingSenderId: "534093962738",
+    appId: "1:534093962738:web:1b5ae28d9cfe6278205f6b",
+    measurementId: "G-XQVJWMTJHZ"
+  };
+  const firebaseApp= firebase.initializeApp(firebaseConfig);
+  const database  = firebaseApp.firestore();
+
+  export default database;
